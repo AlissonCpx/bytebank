29c4bcf6575cc27a0fd07de50b09a63a6393270b
AlissonCpx <alissondeandrade1@gmail.com>
2022-07-27 16:46:24 -0300

    Implementado lista e criado o componente extrato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+
+  transferencias: any[] = [];
+
+  transferi($event) {
+    console.log($event);
+    const transferencia = {...$event, data: new Date()}
+    this.transferencias.push(transferencia);
+  }
 }
